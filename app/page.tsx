ce020e621f385185a88a7312b1e443347b6ad254
ahmed <ahmedatigui001@gmail.com>
2023-09-26 21:29:31 +0100

    added tabs

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
-'use client';
+"use client";
 
-import UseSelect from '../Components/Select';
-import { Button, InputBase, Tabs, Tab, TabScrollButton } from '@mui/material-next';
-import Send from '@mui/icons-material/Send';
+// Components
+import UseSelect from "../Components/Select";
+import { Button, InputBase } from "@mui/material-next";
+import { Tab, tabClasses } from "@mui/base/Tab";
+import { TabsList } from "@mui/base/TabsList";
+import { TabPanel } from "@mui/base/TabPanel";
+import { Tabs } from "@mui/base/Tabs";
+
+// Icons
+import Send from "@mui/icons-material/Send";
 
 export default function Home() {
   return (
@@ -45,9 +52,52 @@ export default function Home() {
           </div>
           <div className="panels">
             <div className="panel | bg-surface-container large-rounding">
+              <Tabs defaultValue={1} className="tabs">
+                <TabsList className="tabs-list">
+                  <Tab value={1} className="tab">
+                    One
+                  </Tab>
+                  <Tab value={2} className="tab">
+                    Two
+                  </Tab>
+                  <Tab value={3} className="tab">
+                    Three
+                  </Tab>
+                </TabsList>
+                <TabPanel value={1} className="tab-panel">
+                  First page
+                </TabPanel>
+                <TabPanel value={2} className="tab-panel">
+                  Second page
+                </TabPanel>
+                <TabPanel value={3} className="tab-panel">
+                  Third page
+                </TabPanel>
+              </Tabs>
             </div>
             <div className="panel | bg-surface-container large-rounding">
-              Response Panel
+              <Tabs defaultValue={1} className="tabs">
+                <TabsList className="tabs-list">
+                  <Tab value={1} className="tab">
+                    One
+                  </Tab>
+                  <Tab value={2} className="tab">
+                    Two
+                  </Tab>
+                  <Tab value={3} className="tab">
+                    Three
+                  </Tab>
+                </TabsList>
+                <TabPanel value={1} className="tab-panel">
+                  First page
+                </TabPanel>
+                <TabPanel value={2} className="tab-panel">
+                  Second page
+                </TabPanel>
+                <TabPanel value={3} className="tab-panel">
+                  Third page
+                </TabPanel>
+              </Tabs>
             </div>
           </div>
         </section>
