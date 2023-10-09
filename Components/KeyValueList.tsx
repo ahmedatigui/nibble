04a5cfa5e4b5de13317650d0b06d6ff6f8ca363f
Ahmed Atigui <ahmedatigui001@gmail.com>
2023-10-09 22:05:10 +0100

    mf: still working on it,hhh

--- Components/KeyValueList.tsx
+++ Components/KeyValueList.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import { useAtom } from 'jotai';
+import { useAtom, atom } from 'jotai';
 
 // Atoms
 import { configParamsAtom, httpRequestConfigAtom } from '@/utils/atoms';
@@ -10,13 +10,21 @@ import { configParamsAtom, httpRequestConfigAtom } from '@/utils/atoms';
 import * as Checkbox from '@radix-ui/react-checkbox';
 import { CheckIcon, TrashIcon } from '@radix-ui/react-icons';
 
+const valueAtom = atom('');
+
 function KeyValueList({ order }: { order: number }) {
   const [fresh, setFresh] = useState(true);
+  const [ke, setKe] = useState('');
+  const [val, setVal] = useAtom(valueAtom);
   const [isKeyValueAllowed, setKeyValueAllowed] = useState(true);
   const [httpRequestConfig, setHttpRequestConfig] = useAtom(httpRequestConfigAtom);
   const [configParams, setConfigParams] = useAtom(configParamsAtom);
 
-  useEffect(() => console.log(isKeyValueAllowed), [isKeyValueAllowed]);
+  useEffect(() => {
+    // updateKey(order, ke);
+    // updateValue(order, val);
+    console.log(configParams, ke, val);
+  }, [ke, val]);
 
   const updateKey = (index: number, newKey: string) => {
     setConfigParams(prevState => ({
@@ -38,7 +46,10 @@ function KeyValueList({ order }: { order: number }) {
     <div className="keyValueListRow">
       <div className="keyValueListInput">
         <input className="key" id="key" placeholder="Key" 
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => updateKey(order, e.target.value)} 
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+            //updateKey(order, e.target.value);
+            setKe(e.target.value)
+          }}
         />
       </div>
       <div className="keyValueListInput">
@@ -46,7 +57,10 @@ function KeyValueList({ order }: { order: number }) {
           className="keyValueListInput value"
           id="value"
           placeholder="Value"
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => updateValue(order, e.target.value)}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+            // updateValue(order, e.target.value);
+            setVal(e.target.value);
+          }}
         />
       </div>
       <div className="keyValueListCheckBox">
